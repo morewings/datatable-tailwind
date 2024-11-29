dc3f0ba4e083ac2daa1ef143709a5da9d4470907
Dima Vyshniakov <morewings@gmail.com>
2024-11-29 19:45:07 +0100

    Part 1: Add table html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import {DataTable} from './DataTable/DataTable.tsx';
+
 const App = () => {
-  return <div className="flex flex-col gap-3 text-amber-600">Hello, world!</div>;
+  return <div><DataTable columns={18} rows={100} /></div>;
 };
 
 export default App;
